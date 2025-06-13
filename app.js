c3114324e19ebc76c6a5dc5cbd7ef34d39bb8796
Lucky-tech10 <ezihegoodluck59@gmail.com>
2025-06-13 13:34:44 +0100

    app.js fix

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// app.get("/", (req, res) => {
-//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-// }
-// );
+app.get("/", (req, res) => {
+  res.send("jobs api");
+  // res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 // Swagger setup
 // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
